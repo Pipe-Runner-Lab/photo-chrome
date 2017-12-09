98fc375f0c6a2905e28b36aa622f9c1a7b1ef0fc
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-12-09 13:41:06 +0530

    created image-editor module

---
+++ src/app/image-editor/image-editor.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CanvasComponent } from './canvas/canvas.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CanvasComponent]
+})
+export class ImageEditorModule { }
